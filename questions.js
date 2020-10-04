e4d7d079c97fa3bfad4ca5a74fc276a1ec0c14cb
Joita Mitra <joita.mitra10@gmail.com>
2020-10-04 21:38:26 +0530

    initial commit

---
+++ questions.js
@@ -0,0 +1,535 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_GB: [
+    {
+      "Which MCU movie featured Spider-Man's first appearance?": [
+        'Captain America: Civil War',
+        'Avengers: Endgame',
+        'Spider-Man: Homecoming',
+        'Avengers: Infinity War',
+        'Avengers: Age of Ultron',
+        'Iron Man 3',
+      ],
+    },
+    {
+      'What does Stark Industries manufacture in Iron Man 1?': [
+        'Weapons',
+        'Chemicals',
+        'Pharmaceuticals',
+        'Buildings',
+        'Cars',
+        'Robots',
+      ],
+    },
+    {
+      'Which of these characters did not appear in Phase One?': [
+        'Black Panther',
+        'Thor',
+        'Captain America',
+        'Pepper Potts',
+        'Black Widow',
+        'Hulk',
+      ],
+    },
+    {
+      'Who played the Hulk in "The Incredible Hulk"?': [
+        'Edward Norton',
+        'Will Smith',
+        'Gary Oldman',
+        'Liam Hemsworth',
+        'Mark Ruffalo',
+        'Josh Brolin'
+      ],
+    },
+    {
+      'Which war did Captain America fight in?': [
+        'World War 2',
+        'World War 1',
+        'Vietnam War',
+        'Iraq War',
+      ],
+    },
+    {
+      "What is the name of Thor's hammer?" : [
+        'Mjolnir',
+        'Vanir',
+        'Aragorn',
+        'Balder',
+        'Aesir',
+      ],
+    },
+    {
+      'Which movie takes place during Christmas time?': [
+        'Iron Man 3',
+        'Thor: The Dark World',
+        'Guardians of the Galaxy',
+        'Ant-Man',
+      ],
+    },
+    {
+      "What is Peter Quill's alter ego?": [
+        'Star-Lord',
+        'Star-Man',
+        'Star-Killer',
+        'Star-Dude',
+      ],
+    },
+    {
+      'What legislation requires superhumans to be governed by law?': [
+        'Sokovia Accords',
+        'Paris Accords',
+        'Latveria Accords',
+        'Slovakia Accords',
+        'Crimea Accords',
+      ],
+    },
+    {
+      'What Nazi organization infiltrated SHIELD?': [
+        'HYDRA',
+        'EMPUSA',
+        'SPHINX',
+        'CHIMERA',
+      ],
+    },
+    {
+      'Where did Scott Lang work before becoming Ant-Man?': [
+        'Baskin Robbins',
+        'Jamba Juice',
+        'KFC',
+        'Chipotle',
+        "McDonald's",
+        'Starbucks',
+      ],
+    },
+    {
+      'What fast food do the Avengers eat after the Battle of New York?': [
+        'Shawarma',
+        'Falafel',
+        'Noodles',
+        'Currywurst',
+        'Burger',
+        'Sushi',
+      ],
+    },
+    {
+      'Which of these characters is not in Doctor Strange?': [
+        'Thanos',
+        'Kaecilius',
+        'Lucian',
+        'Wong',
+        'The Ancient One',
+      ],
+    },
+    {
+      'Who is the main villain in Spider-Man: Homecoming?': [
+        'Vulture',
+        'Venom',
+        'Mysterio',
+        'Kraven',
+        'Green Goblin',
+        'Scorpion',
+      ],
+    },
+    {
+      'What tape does Peter Quill listen to on his Walkman?': [
+        'Awesome Mix Volume 1',
+        'Great Tunes Volume 1',
+        'Get Psyched Mix Volume 1',
+        'Awesome Songs Volume 1',
+      ],
+    },
+    {
+      "Which movie was the Falcon's first appearance?": [
+        'Captain America: The Winter Soldier',
+        'The Avengers',
+        'Avengers: Age of Ultron',
+        'Iron Man 2',
+        'Captain America: The First Avenger',
+      ],
+    },
+    {
+      'Which of these movies came first?': [
+        'Ant-Man',
+        'Guardians of the Galaxy Volume 2',
+        'Captain America: Civil War',
+        'Doctor Strange',
+      ],
+    },
+    {
+      'Who has the Infinity Gauntlet?': [
+        'Thanos',
+        'Helmut Zemo',
+        'The Mandarin',
+        'Ulysses Klaw',
+      ],
+    },
+    {
+      'Who died in Avengers: Age of Ultron?': [
+        'Quicksilver',
+        'Scarlet Witch',
+        'Hawkeye',
+        'Vision',
+      ],
+    },
+    {
+      'Which of these was not an item on Captain America\'s list?': [
+        'Prince',
+        'Steve Jobs (Apple)',
+        'Nirvana(Band)',
+        'Star Wars/Trek',
+        'Moon Landing',
+      ],
+    },
+    {
+      'Which of these MCU characters does not make a cameo appearance in Thor?': [
+        'Black Widow',
+        'Nick Fury',
+        'Hawkeye',
+        'Agent Coulson',
+      ],
+    },
+    {
+      'Which planet are Thor and the Hulk on in Thor: Ragnarok?': [
+        'Sakaar',
+        'Knowhere',
+        'Asgard',
+        'Xandar',
+      ],
+    },
+    {
+      'Who was the main villain in Ant-Man?': [
+        'Yellowjacket',
+        'The Wasp',
+        'Red Skull',
+        'Galactus',
+      ],
+    },
+    {
+      'What type of Giants appear in Thor?': [
+        'Frost Giants',
+        'Storm Giants',
+        'Ice Giants',
+        'Mountain Giants',
+      ],
+    },
+    {
+      'Which infamous Marvel character apeared in the post credits scene of Guardians of the Galaxy?': [
+        'Howard the Duck',
+        'Johnny the Duck',
+        'Ricky the Duck',
+        'Arnie the Duck',
+      ],
+    },
+    {
+      'Who is the main villain in The Incredible Hulk?': [
+        'The Abomination',
+        'Absorbing Man',
+        'The Grandmaster',
+        'The Collector',
+      ],
+    },
+    {
+      'Where does Steve Rogers realize he\'s in the 21st century?': [
+        'Times Square',
+        'Empire State Building',
+        'Wall Street',
+        'Baxter Building',
+      ],
+    },
+    {
+      'Which of these movies was released last?': [
+        'Guardians of the Galaxy Volume 2',
+        'Thor: The Dark World',
+        'Captain America: The Winter Soldier',
+        'Doctor Strange',
+      ],
+    },
+    {
+      'Who played the Mandarin?': [
+        'Ben Kingsley',
+        'Sam Rockwell',
+        'Paul Giamatti',
+        'Mickey Rourke',
+      ],
+    },
+    {
+      'Who has a cameo in every MCU movie?': [
+        'Stan Lee',
+        'Jack Kirby',
+        'Steve Ditko',
+        'Joss Whedon',
+        'Josh Brolin',
+      ],
+    },
+  ],
+  QUESTIONS_EN_US: [
+    {
+      "Which MCU movie featured Spider-Man's first appearance?": [
+        'Captain America: Civil War',
+        'Avengers: Endgame',
+        'Spider-Man: Homecoming',
+        'Avengers: Infinity War',
+        'Avengers: Age of Ultron',
+        'Iron Man 3',
+      ],
+    },
+    {
+      'What does Stark Industries manufacture in Iron Man 1?': [
+        'Weapons',
+        'Chemicals',
+        'Pharmaceuticals',
+        'Buildings',
+        'Cars',
+        'Robots',
+      ],
+    },
+    {
+      'Which of these characters did not appear in Phase One?': [
+        'Black Panther',
+        'Thor',
+        'Captain America',
+        'Pepper Potts',
+        'Black Widow',
+        'Hulk',
+      ],
+    },
+    {
+      'Who played the Hulk in "The Incredible Hulk"?': [
+        'Edward Norton',
+        'Will Smith',
+        'Gary Oldman',
+        'Liam Hemsworth',
+        'Mark Ruffalo',
+        'Josh Brolin'
+      ],
+    },
+    {
+      'Which war did Captain America fight in?': [
+        'World War 2',
+        'World War 1',
+        'Vietnam War',
+        'Iraq War',
+      ],
+    },
+    {
+      "What is the name of Thor's hammer?" : [
+        'Mjolnir',
+        'Vanir',
+        'Aragorn',
+        'Balder',
+        'Aesir',
+      ],
+    },
+    {
+      'Which movie takes place during Christmas time?': [
+        'Iron Man 3',
+        'Thor: The Dark World',
+        'Guardians of the Galaxy',
+        'Ant-Man',
+      ],
+    },
+    {
+      "What is Peter Quill's alter ego?": [
+        'Star-Lord',
+        'Star-Man',
+        'Star-Killer',
+        'Star-Dude',
+      ],
+    },
+    {
+      'What legislation requires superhumans to be governed by law?': [
+        'Sokovia Accords',
+        'Paris Accords',
+        'Latveria Accords',
+        'Slovakia Accords',
+        'Crimea Accords',
+      ],
+    },
+    {
+      'What Nazi organization infiltrated SHIELD?': [
+        'HYDRA',
+        'EMPUSA',
+        'SPHINX',
+        'CHIMERA',
+      ],
+    },
+    {
+      'Where did Scott Lang work before becoming Ant-Man?': [
+        'Baskin Robbins',
+        'Jamba Juice',
+        'KFC',
+        'Chipotle',
+        "McDonald's",
+        'Starbucks',
+      ],
+    },
+    {
+      'What fast food do the Avengers eat after the Battle of New York?': [
+        'Shawarma',
+        'Falafel',
+        'Noodles',
+        'Currywurst',
+        'Burger',
+        'Sushi',
+      ],
+    },
+    {
+      'Which of these characters is not in Doctor Strange?': [
+        'Thanos',
+        'Kaecilius',
+        'Lucian',
+        'Wong',
+        'The Ancient One',
+      ],
+    },
+    {
+      'Who is the main villain in Spider-Man: Homecoming?': [
+        'Vulture',
+        'Venom',
+        'Mysterio',
+        'Kraven',
+        'Green Goblin',
+        'Scorpion',
+      ],
+    },
+    {
+      'What tape does Peter Quill listen to on his Walkman?': [
+        'Awesome Mix Volume 1',
+        'Great Tunes Volume 1',
+        'Get Psyched Mix Volume 1',
+        'Awesome Songs Volume 1',
+      ],
+    },
+    {
+      "Which movie was the Falcon's first appearance?": [
+        'Captain America: The Winter Soldier',
+        'The Avengers',
+        'Avengers: Age of Ultron',
+        'Iron Man 2',
+        'Captain America: The First Avenger',
+      ],
+    },
+    {
+      'Which of these movies came first?': [
+        'Ant-Man',
+        'Guardians of the Galaxy Volume 2',
+        'Captain America: Civil War',
+        'Doctor Strange',
+      ],
+    },
+    {
+      'Who has the Infinity Gauntlet?': [
+        'Thanos',
+        'Helmut Zemo',
+        'The Mandarin',
+        'Ulysses Klaw',
+      ],
+    },
+    {
+      'Who died in Avengers: Age of Ultron?': [
+        'Quicksilver',
+        'Scarlet Witch',
+        'Hawkeye',
+        'Vision',
+      ],
+    },
+    {
+      'Which of these was not an item on Captain America\'s list?': [
+        'Prince',
+        'Steve Jobs (Apple)',
+        'Nirvana(Band)',
+        'Star Wars/Trek',
+        'Moon Landing',
+      ],
+    },
+    {
+      'Which of these MCU characters does not make a cameo appearance in Thor?': [
+        'Black Widow',
+        'Nick Fury',
+        'Hawkeye',
+        'Agent Coulson',
+      ],
+    },
+    {
+      'Which planet are Thor and the Hulk on in Thor: Ragnarok?': [
+        'Sakaar',
+        'Knowhere',
+        'Asgard',
+        'Xandar',
+      ],
+    },
+    {
+      'Who was the main villain in Ant-Man?': [
+        'Yellojacket',
+        'The Wasp',
+        'Red Skull',
+        'Galactus',
+      ],
+    },
+    {
+      'What type of Giants appear in Thor?': [
+        'Frost Giants',
+        'Storm Giants',
+        'Ice Giants',
+        'Mountain Giants',
+      ],
+    },
+    {
+      'Which infamous Marvel character apeared in the post credits scene of Guardians of the Galaxy?': [
+        'Howard the Duck',
+        'Johnny the Duck',
+        'Ricky the Duck',
+        'Arnie the Duck',
+      ],
+    },
+    {
+      'Who is the main villain in The Incredible Hulk?': [
+        'The Abomination',
+        'Absorbing Man',
+        'The Grandmaster',
+        'The Collector',
+      ],
+    },
+    {
+      'Where does Steve Rogers realize he\'s in the 21st century?': [
+        'Times Square',
+        'Empire State Building',
+        'Wall Street',
+        'Baxter Building',
+      ],
+    },
+    {
+      'Which of these movies was released last?': [
+        'Guardians of the Galaxy Volume 2',
+        'Thor: The Dark World',
+        'Captain America: The Winter Soldier',
+        'Doctor Strange',
+      ],
+    },
+    {
+      'Who played the Mandarin?': [
+        'Ben Kingsley',
+        'Sam Rockwell',
+        'Paul Giamatti',
+        'Mickey Rourke',
+      ],
+    },
+    {
+      'Who has a cameo in every MCU movie?': [
+        'Stan Lee',
+        'Jack Kirby',
+        'Steve Ditko',
+        'Joss Whedon',
+        'Josh Brolin',
+      ],
+    },
+  ],
+};
+
+
+
